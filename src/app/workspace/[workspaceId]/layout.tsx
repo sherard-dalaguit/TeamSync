2808d48349dc7b59e64892be95779291cf08e3ff
sherard_dalaguit <sherard.softwaredev@gmail.com>
2024-09-24 13:11:09 -0700

    Add resizable sidebar to workspace layout
    Implemented a resizable sidebar in the workspace layout to provide better user customization. Introduced a `WorkspaceSidebar` component to display workspace details and the `WorkspaceHeader` component for workspace actions. Added a `Hint` component for tooltips used in action buttons.

--- src/app/workspace/[workspaceId]/layout.tsx
+++ src/app/workspace/[workspaceId]/layout.tsx
@@ -3,6 +3,12 @@
 import Toolbar from "@/app/workspace/[workspaceId]/toolbar";
 import {Sidebar} from "@/app/workspace/[workspaceId]/sidebar";
 import React from "react";
+import {
+	ResizableHandle,
+	ResizablePanel,
+	ResizablePanelGroup
+} from "@/components/ui/resizable";
+import {WorkspaceSidebar} from "@/app/workspace/[workspaceId]/workspace-sidebar";
 
 interface WorkspaceIdLayout {
 	children: React.ReactNode;
@@ -14,7 +20,20 @@ const WorkspaceLayout = ({ children }: WorkspaceIdLayout) => {
 			<Toolbar />
 			<div className="flex h-[calc(100vh-40px)]">
 				<Sidebar />
-				{children}
+
+				<ResizablePanelGroup direction="horizontal" autoSaveId="sherard-workspace-layout">
+					<ResizablePanel
+						defaultSize={20}
+						minSize={11}
+						className="bg-[#5E2C5F]"
+					>
+						<WorkspaceSidebar />
+					</ResizablePanel>
+					<ResizableHandle withHandle />
+					<ResizablePanel minSize={20}>
+						{children}
+					</ResizablePanel>
+				</ResizablePanelGroup>
 			</div>
 		</div>
 	);
