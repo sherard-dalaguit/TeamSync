7808ebfd18947bb39c39ab9beae0edcac6b28af2
sherard_dalaguit <sherard.softwaredev@gmail.com>
2024-09-23 23:42:46 -0700

    Add authentication with GitHub, Google, and Password providers
    Integrated new authentication methods from GitHub, Google, and Password via `@convex-dev/auth`. Updated dependencies in `package.json` and created configuration files for authentication setup. This enhances user authentication options and streamlines sign-in processes.

--- src/features/auth/components/sign-in-card.tsx
+++ src/features/auth/components/sign-in-card.tsx
@@ -6,15 +6,38 @@ import {Input} from "@/components/ui/input";
 import {Button} from "@/components/ui/button";
 import {Separator} from "@/components/ui/separator";
 import {SignInFlow} from "@/features/auth/types";
-import {useState} from "react";
+import React, {useState} from "react";
+import {useAuthActions} from "@convex-dev/auth/react";
+import {TriangleAlert} from "lucide-react";
 
 interface SignInCardProps {
 	setState: (state: SignInFlow) => void;
 }
 
 export const SignInCard = ({ setState }: SignInCardProps) => {
+	const { signIn } = useAuthActions();
+
 	const [email, setEmail] = useState("")
 	const [password, setPassword] = useState("")
+	const [pending, setPending] = useState(false)
+	const [error, setError] = useState("")
+
+	const onPasswordSignIn = (e: React.FormEvent<HTMLFormElement>) => {
+		e.preventDefault();
+
+		setPending(true);
+		signIn("password", { email, password, flow: "signIn" })
+			.catch(() => {
+				setError("Invalid email or password");
+			})
+			.finally(() => setPending(false));
+	}
+
+	const onProviderSignIn = (value: "github" | "google") => {
+		setPending(true);
+		signIn(value)
+			.finally(() => setPending(false));
+	}
 
 	return (
 		<Card className="w-full h-full p-8">
@@ -24,11 +47,16 @@ export const SignInCard = ({ setState }: SignInCardProps) => {
 					Use your email or another service to continue
 				</CardDescription>
 			</CardHeader>
-
+			{!!error && (
+				<div className="bg-destructive/15 p-3 rounded-md flex items-center gap-x-2 text-sm text-destructive mb-6">
+					<TriangleAlert className="size-4" />
+					<p>{error}</p>
+				</div>
+			)}
 			<CardContent className="space-y-5 px-0 pb-0">
-				<form className="space-y-2.5">
+				<form onSubmit={onPasswordSignIn} className="space-y-2.5">
 					<Input
-						disabled={false}
+						disabled={pending}
 						value={email}
 						onChange={(e) => setEmail(e.target.value)}
 						placeholder="Email"
@@ -36,22 +64,22 @@ export const SignInCard = ({ setState }: SignInCardProps) => {
 						required
 					/>
 					<Input
-						disabled={false}
+						disabled={pending}
 						value={password}
 						onChange={(e) => setPassword(e.target.value)}
 						placeholder="Password"
 						type="password"
 						required
 					/>
-					<Button type="submit" className="w-full" size="lg" disabled={false}>
+					<Button type="submit" className="w-full" size="lg" disabled={pending}>
 						Continue
 					</Button>
 				</form>
 				<Separator />
 				<div className="flex flex-col gap-y-2.5">
 					<Button
-						disabled={false}
-						onClick={() => {}}
+						disabled={pending}
+						onClick={() => onProviderSignIn("google")}
 						variant="outline"
 						size="lg"
 						className="w-full relative"
@@ -60,8 +88,8 @@ export const SignInCard = ({ setState }: SignInCardProps) => {
 						Continue with Google
 					</Button>
 					<Button
-						disabled={false}
-						onClick={() => {}}
+						disabled={pending}
+						onClick={() => onProviderSignIn("github")}
 						variant="outline"
 						size="lg"
 						className="w-full relative"
