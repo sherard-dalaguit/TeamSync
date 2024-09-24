eb985ba6c8d4d1fba286d56cac049e947be85660
sherard_dalaguit <sherard.softwaredev@gmail.com>
2024-09-24 07:02:21 -0700

    Add Shadcn UI avatar and dropdown menu dependencies
    Added dependencies for `@shadcn/react-avatar` and `@shadcn/react-dropdown-menu` to `package.json` and updated relevant entries in `package-lock.json`. These enhancements are essential for upcoming UI improvements requiring these Radix components.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,11 @@
 "use client";
 
-import { useAuthActions } from "@convex-dev/auth/react"
-import {Button} from "@/components/ui/button";
+import {UserButton} from "@/features/auth/components/user-button";
 
 export default function Home() {
-	const { signOut } = useAuthActions();
-
 	return (
 		<div>
-			Logged in
-			<Button onClick={() => signOut()}>Sign Out</Button>
+			<UserButton />
 		</div>
 	);
 }
