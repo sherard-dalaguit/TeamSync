dcde28b6bda327b9d68201b39a76274baa969a77
sherard_dalaguit <sherard.softwaredev@gmail.com>
2024-09-23 21:54:55 -0700

    Add initial Next.js project files
    Created `next.config.mjs`, `package.json`, and `package-lock.json` to set up a basic Next.js project. The configuration includes scripts for development, build, and start, along with dependencies for React, TailwindCSS, and some utility libraries. This sets the foundation for the TeamSync application.

---
+++ src/app/page.tsx
@@ -0,0 +1,9 @@
+import {Button} from "@/components/ui/button";
+
+export default function Home() {
+	return (
+		<div className="text-bold text-2xl text-rose-500">
+			<Button>Click me</Button>
+		</div>
+	);
+}
