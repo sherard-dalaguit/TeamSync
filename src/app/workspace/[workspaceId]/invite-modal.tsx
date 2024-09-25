1cb9ba03dc2c8974bbb30f9bfee601a7dba251cb
sherard_dalaguit <sherard.softwaredev@gmail.com>
2024-09-25 12:53:21 -0700

    Add invite modal to workspace header
    Implemented an InviteModal component to facilitate user invitations to workspaces. Updated workspace-header.tsx to include the InviteModal, with functionality to copy invite links to the clipboard.

---
+++ src/app/workspace/[workspaceId]/invite-modal.tsx
@@ -0,0 +1,47 @@
+import {Dialog, DialogContent, DialogDescription, DialogHeader, DialogTitle} from "@/components/ui/dialog";
+import {Button} from "@/components/ui/button";
+import {CopyIcon} from "lucide-react";
+import {useWorkspaceId} from "@/hooks/use-workspace-id";
+import {toast} from "sonner";
+
+interface InviteModalProps {
+	open: boolean;
+	setOpen: (open: boolean) => void;
+	name: string;
+	joinCode: string;
+}
+
+export const InviteModal = ({open, setOpen, name, joinCode}: InviteModalProps) => {
+	const workspaceId = useWorkspaceId();
+	const handleCopy = () => {
+		const inviteLink = `${window.location.origin}/join/${workspaceId}`;
+
+		navigator.clipboard.writeText(inviteLink).then(() => toast.success("Invite link copied to clipboard"));
+	}
+
+	return (
+		<Dialog open={open} onOpenChange={setOpen}>
+			<DialogContent>
+				<DialogHeader>
+					<DialogTitle>Invite people to {name}</DialogTitle>
+					<DialogDescription>
+						Use the code below to invite people to your workspace
+					</DialogDescription>
+				</DialogHeader>
+				<div className="flex flex-col gap-y-4 items-center justify-center py-10">
+					<p className="text-4xl font-bold tracking-widest uppercase">
+						{joinCode}
+					</p>
+					<Button
+						onClick={handleCopy}
+						variant="ghost"
+						size="sm"
+					>
+						Copy link
+						<CopyIcon className="size-4 ml-2" />
+					</Button>
+				</div>
+			</DialogContent>
+		</Dialog>
+	)
+}
--- src/app/workspace/[workspaceId]/workspace-header.tsx
+++ src/app/workspace/[workspaceId]/workspace-header.tsx
@@ -11,6 +11,7 @@ import {ChevronDown, ListFilter, SquarePen} from "lucide-react";
 import {Hint} from "@/components/hint";
 import {PreferencesModal} from "@/app/workspace/[workspaceId]/preferences-modal";
 import {useState} from "react";
+import {InviteModal} from "@/app/workspace/[workspaceId]/invite-modal";
 
 interface WorkspaceHeaderProps {
 	workspace: Doc<"workspaces">;
@@ -19,9 +20,16 @@ interface WorkspaceHeaderProps {
 
 export const WorkspaceHeader = ({ workspace, isAdmin }: WorkspaceHeaderProps) => {
 	const [preferencesOpen, setPreferencesOpen] = useState(false);
+	const [inviteOpen, setInviteOpen] = useState(false);
 
 	return (
 		<>
+			<InviteModal
+				open={inviteOpen}
+				setOpen={setInviteOpen}
+				name={workspace.name}
+				joinCode={workspace.joinCode}
+			/>
 			<PreferencesModal open={preferencesOpen} setOpen={setPreferencesOpen} initialValue={workspace.name} />
 			<div className="flex items-center justify-between px-4 h-[49px] gap-0.5">
 				<DropdownMenu>
@@ -50,7 +58,7 @@ export const WorkspaceHeader = ({ workspace, isAdmin }: WorkspaceHeaderProps) =>
 								<DropdownMenuSeparator />
 								<DropdownMenuItem
 									className="cursor-pointer py-2"
-									onClick={() => {}}
+									onClick={() => setInviteOpen(true)}
 								>
 									Invite people to {workspace.name}
 								</DropdownMenuItem>
