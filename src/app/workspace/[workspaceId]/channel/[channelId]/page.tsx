dd5be2c1383077b619d6b6ba01aaf95936f302eb
sherard_dalaguit <sherard.softwaredev@gmail.com>
2024-09-26 09:05:17 -0700

    Enhance workspace and channel loading states
    Added memberLoading checks in workspace page for comprehensive loading states. Introduced new header component in channel page to handle channel details and actions, like renaming and deletion, providing a better user interface and improved functionality.

--- src/app/workspace/[workspaceId]/channel/[channelId]/page.tsx
+++ src/app/workspace/[workspaceId]/channel/[channelId]/page.tsx
@@ -1,9 +1,35 @@
-import React from 'react';
+"use client";
+
+import {useChannelId} from "@/hooks/use-channel-id";
+import {useGetChannel} from "@/features/channels/api/use-get-channel";
+import {Loader, TriangleAlert} from "lucide-react";
+import {Header} from "@/app/workspace/[workspaceId]/channel/[channelId]/header";
 
 const ChannelIdPage = () => {
+	const channelId = useChannelId();
+
+	const { data: channel, isLoading: channelLoading } = useGetChannel({ id: channelId });
+
+	if (channelLoading) {
+		return (
+			<div className="h-full flex-1 flex items-center justify-center">
+				<Loader className="size-5 text-muted-foreground animate-spin" />
+			</div>
+		)
+	}
+
+	if (!channel) {
+		return (
+			<div className="h-full flex-1 flex flex-col gap-y-2 items-center justify-center">
+				<TriangleAlert className="size-5 text-muted-foreground" />
+				<span className="text-sm text-muted-foreground">Channel not found</span>
+			</div>
+		)
+	}
+
 	return (
-		<div>
-			Channel Id page
+		<div className="flex flex-col h-full">
+			<Header title={channel.name} />
 		</div>
 	);
 };
