0d2c64890478d4f9fdb605c2af262c87d7e473b2
sherard_dalaguit <sherard.softwaredev@gmail.com>
2024-09-29 07:17:59 -0700

    Add image upload functionality to ChatInput
    Introduced a new hook `useGenerateUploadUrl` to handle the generation of upload URLs. Modified `ChatInput` component to upload images and include image storage ID in the message payload.

--- convex/messages.ts
+++ convex/messages.ts
@@ -16,6 +16,7 @@ export const create = mutation({
 		image: v.optional(v.id("_storage")),
 		workspaceId: v.id("workspaces"),
 		channelId: v.optional(v.id("channels")),
+		conversationId: v.optional(v.id("conversations")),
 		parentMessageId: v.optional(v.id("messages")),
 	},
 	handler: async (ctx, args) => {
@@ -31,11 +32,25 @@ export const create = mutation({
 			throw new Error("Unauthorized");
 		}
 
+		let _conversationId = args.conversationId;
+
+		// only possible if we are replying in a 1:1 conversation
+		if (!args.conversationId && !args.channelId && args.parentMessageId) {
+			const parentMessage = await ctx.db.get(args.parentMessageId);
+
+			if (!parentMessage) {
+				throw new Error("Parent message not found");
+			}
+
+			_conversationId = parentMessage.conversationId;
+		}
+
 		const messageId = await ctx.db.insert("messages", {
 			memberId: member._id,
 			body: args.body,
 			image: args.image,
 			channelId: args.channelId,
+			conversationId: _conversationId,
 			workspaceId: args.workspaceId,
 			parentMessageId: args.parentMessageId,
 			updatedAt: Date.now()
