30957b202a7931ceb18a2a679c9c1dc0ddda8f95
sherard_dalaguit <sherard.softwaredev@gmail.com>
2024-09-29 08:21:52 -0700

    Add message retrieval functionality
    Integrate message fetching with `useGetMessages` hook in channel page. Update database schema to include necessary indices. Implement message retrieval logic, reactions population, and thread population within `messages.ts`.

--- src/app/workspace/[workspaceId]/channel/[channelId]/page.tsx
+++ src/app/workspace/[workspaceId]/channel/[channelId]/page.tsx
@@ -5,10 +5,12 @@ import {useGetChannel} from "@/features/channels/api/use-get-channel";
 import {Loader, TriangleAlert} from "lucide-react";
 import {Header} from "@/app/workspace/[workspaceId]/channel/[channelId]/header";
 import {ChatInput} from "@/app/workspace/[workspaceId]/channel/[channelId]/chat-input";
+import {useGetMessages} from "@/features/messages/api/use-get-messages";
 
 const ChannelIdPage = () => {
 	const channelId = useChannelId();
 
+	const { results } = useGetMessages({ channelId });
 	const { data: channel, isLoading: channelLoading } = useGetChannel({ id: channelId });
 
 	if (channelLoading) {
@@ -31,7 +33,9 @@ const ChannelIdPage = () => {
 	return (
 		<div className="flex flex-col h-full">
 			<Header title={channel.name} />
-			<div className="flex-1" />
+			<div className="flex-1">
+				{JSON.stringify(results)}
+			</div>
 			<ChatInput placeholder={`Message # ${channel.name}`}/>
 		</div>
 	);
