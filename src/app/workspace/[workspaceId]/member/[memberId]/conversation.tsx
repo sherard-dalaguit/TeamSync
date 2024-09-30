50f994014f0c14607674dc2b137ff64e15836e63
sherard_dalaguit <sherard.softwaredev@gmail.com>
2024-09-30 08:56:32 -0700

    Add conversation feature for workspace members
    Implemented a new conversation component allowing workspace members to have individual chats. This includes UI elements for avatars, member-specific messages, and loading states. Added API handling for creating or retrieving conversations.

---
+++ src/app/workspace/[workspaceId]/member/[memberId]/conversation.tsx
@@ -0,0 +1,53 @@
+import {Id} from "../../../../../../convex/_generated/dataModel";
+import {useMemberId} from "@/hooks/use-member-id";
+import {useGetMember} from "@/features/members/api/use-get-member";
+import {useGetMessages} from "@/features/messages/api/use-get-messages";
+import {Loader} from "lucide-react";
+import {Header} from "@/app/workspace/[workspaceId]/member/[memberId]/header";
+import {ChatInput} from "@/app/workspace/[workspaceId]/member/[memberId]/chat-input";
+import {MessageList} from "@/components/message-list";
+
+interface ConversationProps {
+	id: Id<"conversations">
+}
+
+export const Conversation = ({ id }: ConversationProps) => {
+	const memberId = useMemberId();
+
+	const { data: member, isLoading: memberLoading } = useGetMember({ id: memberId});
+	const { results, status, loadMore } = useGetMessages({
+		conversationId: id
+	});
+
+	if (memberLoading || status === "LoadingFirstPage") {
+		return (
+			<div className="h-full flex-1 flex items-center justify-center">
+				<Loader className="size-6 text-muted-foreground animate-spin"/>
+			</div>
+		)
+	}
+
+	return (
+		<div className="flex flex-col h-full">
+			<Header
+				memberName={member?.user.name}
+				memberImage={member?.user.image}
+				onClick={() => {}}
+			/>
+			<MessageList
+				data={results}
+				variant="conversation"
+				memberImage={member?.user.image}
+				memberName={member?.user.name}
+				loadMore={loadMore}
+				isLoadingMore={status === "LoadingMore"}
+				canLoadMore={status === "CanLoadMore"}
+			/>
+			<ChatInput
+				placeholder={`Message ${member?.user.name}`}
+				conversationId={id}
+			/>
+
+		</div>
+	)
+}
