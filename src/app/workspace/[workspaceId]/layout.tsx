7fb194b67385dddd5012629716e2a7071a3e293c
sherard_dalaguit <sherard.softwaredev@gmail.com>
2024-09-30 07:34:43 -0700

    Add thread component for message details in workspace layout
    Introduced a new Thread component to handle the display of detailed message threads. Modified the workspace layout to include a resizable panel that shows the thread when a message is selected. This enhances the user experience by allowing in-depth message interactions within the workspace.

--- src/app/workspace/[workspaceId]/layout.tsx
+++ src/app/workspace/[workspaceId]/layout.tsx
@@ -9,12 +9,20 @@ import {
 	ResizablePanelGroup
 } from "@/components/ui/resizable";
 import {WorkspaceSidebar} from "@/app/workspace/[workspaceId]/workspace-sidebar";
+import {usePanel} from "@/hooks/use-panel";
+import {Loader} from "lucide-react";
+import {Id} from "../../../../convex/_generated/dataModel";
+import {Thread} from "@/features/messages/components/thread";
 
 interface WorkspaceIdLayout {
 	children: React.ReactNode;
 }
 
 const WorkspaceLayout = ({ children }: WorkspaceIdLayout) => {
+	const { parentMessageId, onClose } = usePanel();
+
+	const showPanel = !!parentMessageId
+
 	return (
 		<div className="h-full">
 			<Toolbar />
@@ -33,6 +41,23 @@ const WorkspaceLayout = ({ children }: WorkspaceIdLayout) => {
 					<ResizablePanel minSize={20}>
 						{children}
 					</ResizablePanel>
+					{showPanel && (
+						<>
+							<ResizableHandle withHandle />
+							<ResizablePanel minSize={20} defaultSize={29}>
+								{parentMessageId ? (
+									<Thread
+										messageId={parentMessageId as Id<"messages">}
+										onClose={onClose}
+									/>
+								) : (
+									<div className="flex h-full items-center justify-center">
+										<Loader className="size-5 animate-spin text-muted-foreground"/>
+									</div>
+								)}
+							</ResizablePanel>
+						</>
+					)}
 				</ResizablePanelGroup>
 			</div>
 		</div>
