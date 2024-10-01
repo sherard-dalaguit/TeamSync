60f0b6aa463cfe9a3384641484cc7b14ebff75ad
sherard_dalaguit <sherard.softwaredev@gmail.com>
2024-10-01 11:54:29 -0700

    Change background colors for consistency
    Updated background colors in sidebar, toolbar, and workspace-sidebar components to ensure a consistent dark theme across the workspace. This enhances the visual cohesiveness and improves the user interface experience.

--- src/app/workspace/[workspaceId]/sidebar.tsx
+++ src/app/workspace/[workspaceId]/sidebar.tsx
@@ -8,7 +8,7 @@ export const Sidebar = () => {
 	const pathname = usePathname();
 
 	return (
-		<aside className="w-[70px] h-full bg-[#481349] flex flex-col gap-y-4 items-center pt-[9px] pb-4">
+		<aside className="w-[70px] h-full bg-[#2B2B2B] flex flex-col gap-y-4 items-center pt-[9px] pb-4">
 			<WorkspaceSwitcher />
 			<SidebarButton icon={Home} label="Home" isActive={pathname.includes('/workspace')} />
 			<SidebarButton icon={MessagesSquare} label="DMs" />
--- src/app/workspace/[workspaceId]/workspace-sidebar.tsx
+++ src/app/workspace/[workspaceId]/workspace-sidebar.tsx
@@ -27,7 +27,7 @@ export const WorkspaceSidebar = () => {
 
 	if (workspaceLoading || memberLoading) {
 		return (
-			<div className="flex flex-col bg-[#5E2C5F] h-full items-center justify-center">
+			<div className="flex flex-col bg-[#333333] h-full items-center justify-center">
 				<Loader className="size-5 animate-spin text-white" />
 			</div>
 		)
@@ -35,7 +35,7 @@ export const WorkspaceSidebar = () => {
 
 	if (!workspace || !member) {
 		return (
-			<div className="flex flex-col gap-y-2 bg-[#5E2C5F] h-full items-center justify-center">
+			<div className="flex flex-col gap-y-2 bg-[#333333] h-full items-center justify-center">
 				<AlertTriangle className="size-5 text-white" />
 				<p className="text-white text-sm">
 					Workspace not found
@@ -45,7 +45,7 @@ export const WorkspaceSidebar = () => {
 	}
 
 	return (
-		<div className="flex flex-col bg-[#5E2C5F] h-full">
+		<div className="flex flex-col bg-[#333333] h-full">
 			<WorkspaceHeader workspace={workspace} isAdmin={member.role === "admin"} />
 
 			<div className="flex flex-col px-2 mt-3">
