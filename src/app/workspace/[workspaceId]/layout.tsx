637a0f3e2fcf4f031746fbadf69f0e05a5c476c4
sherard_dalaguit <sherard.softwaredev@gmail.com>
2024-10-01 10:34:42 -0700

    Add profile component to members' section
    Created a new `Profile` component to display member details, including avatar, name, and contact information. Integrated this component into the workspace layout to toggle visibility based on the presence of a `profileMemberId`.

--- src/app/workspace/[workspaceId]/layout.tsx
+++ src/app/workspace/[workspaceId]/layout.tsx
@@ -13,15 +13,16 @@ import {usePanel} from "@/hooks/use-panel";
 import {Loader} from "lucide-react";
 import {Id} from "../../../../convex/_generated/dataModel";
 import {Thread} from "@/features/messages/components/thread";
+import {Profile} from "@/features/members/components/profile";
 
 interface WorkspaceIdLayout {
 	children: React.ReactNode;
 }
 
 const WorkspaceLayout = ({ children }: WorkspaceIdLayout) => {
-	const { parentMessageId, onClose } = usePanel();
+	const { parentMessageId, profileMemberId, onClose } = usePanel();
 
-	const showPanel = !!parentMessageId
+	const showPanel = !!parentMessageId || !!profileMemberId;
 
 	return (
 		<div className="h-full">
@@ -50,7 +51,12 @@ const WorkspaceLayout = ({ children }: WorkspaceIdLayout) => {
 										messageId={parentMessageId as Id<"messages">}
 										onClose={onClose}
 									/>
-								) : (
+								) : profileMemberId ? (
+									<Profile
+										memberId={profileMemberId as Id<"members">}
+										onClose={onClose}
+									/>
+									) : (
 									<div className="flex h-full items-center justify-center">
 										<Loader className="size-5 animate-spin text-muted-foreground"/>
 									</div>
