740beabb4985295caf46d3303919fbb13a9c7cc4
sherard_dalaguit <sherard.softwaredev@gmail.com>
2024-10-03 10:25:11 -0700

    Update background color in auth-screen component
    Change the background color from #5C3B58 to #2B2B2B for better contrast and improved readability. This aligns with the updated design specifications.

--- src/features/auth/components/auth-screen.tsx
+++ src/features/auth/components/auth-screen.tsx
@@ -9,7 +9,7 @@ export const AuthScreen = () => {
 	const [state, setState] = useState<SignInFlow>("signIn");
 
 	return (
-		<div className="h-full flex items-center justify-center bg-[#5C3B58]">
+		<div className="h-full flex items-center justify-center bg-[#2B2B2B]">
 			<div className="mg:h-auto md:w-[420px]">
 				{state === "signIn" ? <SignInCard setState={setState} /> : <SignUpCard setState={setState} />}
 			</div>
